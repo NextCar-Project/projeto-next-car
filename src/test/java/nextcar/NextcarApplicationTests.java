f8ec8caee9013f5796322a88913c73af18eaaf88
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-01 15:33:53 -0300

    setup inicial do projeto

---
+++ src/test/java/nextcar/NextcarApplicationTests.java
@@ -0,0 +1,13 @@
+package nextcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
