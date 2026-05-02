47e2e22e486ce074463e741a3fc9c3fceb4ca07e
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-01 23:28:06 -0300

    adicionando a classe Veiculo

---
+++ src/main/java/nextcar/model/Veiculo.java
@@ -0,0 +1,21 @@
+package nextcar.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String marca;
+    private String modelo;
+    private Integer ano;
+    private Double preco;
+    private String tipo;
+}
