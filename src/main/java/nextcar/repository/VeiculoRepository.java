b9b24bc6617a757c44d67ab5e83043ea9e7d03f7
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-02 22:50:20 -0300

    atualizando a classe VeiculoRepository com uma anotacao

--- src/main/java/nextcar/repository/VeiculoRepository.java
+++ src/main/java/nextcar/repository/VeiculoRepository.java
@@ -2,6 +2,8 @@ package nextcar.repository;
 
 import nextcar.model.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
 }
