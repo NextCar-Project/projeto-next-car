341ed1b334038a1189e94d56c6c6d322099ebabf
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-06 22:19:41 -0300

    conectando ao banco de dados

--- src/main/java/nextcar/repository/UsuarioRepository.java
+++ src/main/java/nextcar/repository/UsuarioRepository.java
@@ -1,4 +1,9 @@
 package nextcar.repository;
 
-public interface UsuarioRepository {
+import nextcar.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
