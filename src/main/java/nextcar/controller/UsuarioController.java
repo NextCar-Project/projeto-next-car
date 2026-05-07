5f5231d2c29f33f76939b1d2f14be415ae65d489
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-06 22:37:30 -0300

    criando a rota (find)

--- src/main/java/nextcar/controller/UsuarioController.java
+++ src/main/java/nextcar/controller/UsuarioController.java
@@ -3,9 +3,12 @@ package nextcar.controller;
 
 import nextcar.model.Usuario;
 import nextcar.service.UsuarioService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -19,5 +22,10 @@ public class UsuarioController {
     @PostMapping("inserir")
     public Usuario save (Usuario usuario) {
         return service.save(usuario);
+    }
+
+    @GetMapping("mostrar")
+    public List<Usuario> find (Usuario usuario) {
+        return service.find(usuario);
     }
 }
