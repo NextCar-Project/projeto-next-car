375f3e0403d7331916b60a1281e52b8da9494fb4
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-06 22:30:54 -0300

    criando a rota (save)

--- src/main/java/nextcar/controller/UsuarioController.java
+++ src/main/java/nextcar/controller/UsuarioController.java
@@ -1,4 +1,23 @@
 package nextcar.controller;
 
+
+import nextcar.model.Usuario;
+import nextcar.service.UsuarioService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+    private final UsuarioService service;
+
+    public UsuarioController (UsuarioService service) {
+        this.service = service;
+    }
+
+    @PostMapping("inserir")
+    public Usuario save (Usuario usuario) {
+        return service.save(usuario);
+    }
 }
