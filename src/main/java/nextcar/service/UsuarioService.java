ce95221024018112df6ee8bbc2a318538f451bc3
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-06 22:26:13 -0300

    criando a funcao (save)

--- src/main/java/nextcar/service/UsuarioService.java
+++ src/main/java/nextcar/service/UsuarioService.java
@@ -1,4 +1,18 @@
 package nextcar.service;
 
+import nextcar.model.Usuario;
+import nextcar.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+    private final UsuarioRepository repository;
+
+    public UsuarioService (UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public Usuario save (Usuario usuario) {
+       return repository.save(usuario);
+    }
 }
