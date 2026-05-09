a8dc8e86358e3d92ac913d3a122b57436d83c05b
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-09 00:33:14 -0300

    modificando a classe SenhaSegura

--- src/main/java/nextcar/configuration/SenhaSegura.java
+++ src/main/java/nextcar/configuration/SenhaSegura.java
@@ -1,7 +1,13 @@
 package nextcar.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SenhaSegura {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
