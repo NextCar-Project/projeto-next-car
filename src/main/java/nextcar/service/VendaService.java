e7c3a35d456eacd4616fb3c8ec4a3036a6e96202
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-11 23:51:21 -0300

    save refatorado

--- src/main/java/nextcar/service/VendaService.java
+++ src/main/java/nextcar/service/VendaService.java
@@ -1,7 +1,11 @@
 package nextcar.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import nextcar.model.Usuario;
+import nextcar.model.Veiculo;
 import nextcar.model.Venda;
+import nextcar.repository.UsuarioRepository;
+import nextcar.repository.VeiculoRepository;
 import nextcar.repository.VendaRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +14,21 @@ import java.util.List;
 @Service
 public class VendaService {
     private final VendaRepository repository;
+    private final VeiculoRepository veiculoRepository;
+    private final UsuarioRepository usuarioRepository;
 
-    public VendaService(VendaRepository repository) {
+    public VendaService(VendaRepository repository, VeiculoRepository veiculoRepository, UsuarioRepository usuarioRepository) {
         this.repository = repository;
+        this.veiculoRepository = veiculoRepository;
+        this.usuarioRepository = usuarioRepository;
     }
 
     public Venda save(Venda venda) {
+       Veiculo veiculo = veiculoRepository.findById(venda.getVeiculo().getId()).orElseThrow(() -> new EntityNotFoundException("Veiculo não encontrado"));
+       Usuario usuario = usuarioRepository.findById(venda.getUsuario().getId()).orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado"));
+
+        venda.setVeiculo(veiculo);
+        venda.setUsuario(usuario);
         return repository.save(venda);
     }
 
