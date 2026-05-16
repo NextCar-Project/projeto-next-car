481761b7eb17a31e37b684608eefd176d71cbcaa
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-15 21:24:46 -0300

    adicionando @Component("normal") na classe PrecoNormal

--- src/main/java/nextcar/service/PrecoNormal.java
+++ src/main/java/nextcar/service/PrecoNormal.java
@@ -1,5 +1,8 @@
 package nextcar.service;
 
+import org.springframework.stereotype.Component;
+
+@Component("normal")
 public class PrecoNormal implements PrecoStrategy{
     @Override
     public double calcularPreco(double valor) {
