cd8e175c97d65a06c4c04434ee00fca2fbd9a21a
Emerson Willian <emersonwdias2013@gmail.com>
2026-05-15 21:55:15 -0300

    Na classe VendaService o metodo buscarPorId adicionado

--- src/main/java/nextcar/service/VendaService.java
+++ src/main/java/nextcar/service/VendaService.java
@@ -1,11 +1,7 @@
 package nextcar.service;
 
 import jakarta.persistence.EntityNotFoundException;
-import nextcar.model.Usuario;
-import nextcar.model.Veiculo;
 import nextcar.model.Venda;
-import nextcar.repository.UsuarioRepository;
-import nextcar.repository.VeiculoRepository;
 import nextcar.repository.VendaRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,40 +10,32 @@ import java.util.List;
 @Service
 public class VendaService {
     private final VendaRepository vendaRepository;
-    private final VeiculoRepository veiculoRepository;
-    private final UsuarioRepository usuarioRepository;
 
-    public VendaService(VendaRepository vendaRepository, VeiculoRepository veiculoRepository, UsuarioRepository usuarioRepository) {
+    public VendaService(VendaRepository vendaRepository) {
         this.vendaRepository = vendaRepository;
-        this.veiculoRepository = veiculoRepository;
-        this.usuarioRepository = usuarioRepository;
     }
 
     public Venda save(Venda venda) {
-       Veiculo veiculo = veiculoRepository.findById(venda.getVeiculo().getId()).orElseThrow(() -> new EntityNotFoundException("Veiculo não encontrado"));
-       Usuario usuario = usuarioRepository.findById(venda.getUsuario().getId()).orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado"));
-
-        venda.setVeiculo(veiculo);
-        venda.setUsuario(usuario);
         return vendaRepository.save(venda);
     }
 
-    public List<Venda> find(){
+    public List<Venda> find() {
         return vendaRepository.findAll();
     }
 
     public Venda update(Venda vendaNova, Long id) {
-        Venda vendaExiste = vendaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Venda não encontrada"));
-
-        vendaExiste.setData(vendaNova.getData());
-        vendaExiste.setValorFinal(vendaNova.getValorFinal());
-        vendaExiste.setVeiculo(vendaNova.getVeiculo());
-
-        return vendaRepository.save(vendaExiste);
+        Venda existe = buscarPorId(id);
+        existe.setData(vendaNova.getData());
+        existe.setValorFinal(vendaNova.getValorFinal());
+        existe.setVeiculo(vendaNova.getVeiculo());
+        return vendaRepository.save(existe);
     }
 
-    public void delete(Long id){
-        Venda venda = vendaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Venda não encontrada"));
-        vendaRepository.delete(venda);
+    public void delete(Long id) {
+        vendaRepository.delete(buscarPorId(id));
+    }
+
+    public Venda buscarPorId(Long id) {
+        return vendaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Venda nao encontrada: " + id));
     }
 }
